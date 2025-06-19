c20acc5467172ad7669e07a9ab7656ec46281c4a
JourderJottle <jamescfar@gmail.com>
2025-06-19 13:44:28 -0400

    setting initial pose

--- asv_arov_navigation/arov_navigation_server.py
+++ asv_arov_navigation/arov_navigation_server.py
@@ -3,29 +3,29 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
 from nav2_simple_commander.robot_navigator import BasicNavigator
-from asv_arov_interfaces.action import NavigationAction
+from asv_arov_interfaces.action import AROVCommandAction
 
 class AROVNavigationServer(Node) :
     def __init__(self) :
         super().__init__('AROV_navigation_server')
-        self.action_server = ActionServer(self, NavigationAction, 'AROV_navigation_action', self.execute_callback_async)
+        self.action_server = ActionServer(self, AROVCommandAction, 'AROV_navigation_action', self.execute_callback_async)
         self.navigator = BasicNavigator(namespace='arov')
         self.navigator.waitUntilNav2Active(localizer="/arov/pose_publisher")
 
     def execute_callback_async(self, goal_handle) :
         if goal_handle.request.mode == 1 :
-            target_pose = goal_handle.request.goal
-            self.navigator.goToPose(target_pose)
+            self.navigator.setInitialPose(goal_handle.request.init_pose)
+            self.navigator.goToPose(goal_handle.request.goal)
             while not self.navigator.isTaskComplete() :
-                feedback = NavigationAction.Feedback()
+                feedback = AROVCommandAction.Feedback()
                 feedback.current_pose = self.navigator.getFeedback().current_pose
                 goal_handle.publish_feedback(feedback)
-            result = NavigationAction.Result()
+            result = AROVCommandAction.Result()
             result.goal_reached = True if self.navigator.getResult().error_code == 0 else False
             return result
         else :
             self.navigator.cancelTask()
-            result = NavigationAction.Result()
+            result = AROVCommandAction.Result()
             result.goal_reached = False
             return result
     
